86f774bd357439e251d70ead8ccdde108d3558ec
bntrtm <git.bntrtm@tjbainter.com>
2026-01-19 23:17:16 -0600

    feat: add basic gemini agent

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    print("Hello from build-ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("MISSING ENV VARIABLE: 'GEMINI_API_KEY'")
+    client = genai.Client(api_key=api_key)
+    resp = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages,
+    )
+    if resp.usage_metadata is None:
+        raise RuntimeError("API RESPONSE FAILED")
+    if args.verbose:
+        print("Prompt tokens: " + str(resp.usage_metadata.prompt_token_count))
+        print("Response tokens: " + str(resp.usage_metadata.candidates_token_count))
+        print("User prompt: " + resp.text)
+    else:
+        print(resp.text)
+
+
+if __name__ == "__main__":
+    main()
